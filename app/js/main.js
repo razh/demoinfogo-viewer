021b4fc06f6d195260a25d2684a4e96c34358663
Raymond Zhou <somecrazyidiot@gmail.com>
2014-11-27 03:18:26 -0500

    Add skeleton of command parsing loop.

--- app/js/main.js
+++ app/js/main.js
@@ -12,6 +12,28 @@ console.log(messages);
 
 const MAX_OSPATH = 260;
 
+const DemoMessage = {
+  // Startup message. Process as fast as possible.
+  DEM_SIGNON: 1,
+  // Normal network packet that is stored off.
+  DEM_PACKET: 2,
+  // Sync client clock to demo tick.
+  DEM_SYNCTICK: 3,
+  // Console command.
+  DEM_CONSOLECMD: 4,
+  // User input command.
+  DEM_USERCMD: 5,
+  // Network data tables.
+  DEM_DATATABLES: 6,
+  // End of time.
+  DEM_STOP: 7,
+  // A blob of binary data understood by a callback function.
+  DEM_CUSTOMDATA: 8,
+  DEM_STRINGTABLES: 9,
+  // Last command. Same as DEM_STRINGTABLES.
+  DEM_LASTCMD: 9
+};
+
 document.addEventListener( 'drop', event => {
   event.stopPropagation();
   event.preventDefault();
@@ -30,6 +52,8 @@ document.addEventListener( 'drop', event => {
     return events.map( event => {
       var result = event.srcElement.result;
       var buffer = new Buffer( new Uint8Array( result ) );
+      var length = buffer.length;
+
       var offset = 8;
       // Demo filestamp. Should be HL2DEMO.
       console.log( buffer.toString( 'ascii', 0, offset ) );
@@ -63,6 +87,70 @@ document.addEventListener( 'drop', event => {
       // Sign-on length.
       console.log( buffer.readInt32LE( offset ) );
       offset += 4;
+
+      function readRawData() {
+        // Size.
+        var size = buffer.readInt32LE( offset );
+        offset += 4;
+        console.log( 'size', size );
+        offset += size;
+      }
+
+      console.log( 'commands' );
+      while ( offset < length ) {
+        // Read command header.
+        // Command.
+        var command = buffer.readUInt8( offset );
+        console.log( command );
+        offset++;
+
+        // Time stamp.
+        console.log( buffer.readInt32LE( offset ) );
+        offset += 4;
+
+        // Player slot.
+        console.log( buffer.readUInt8( offset ) );
+        offset++;
+
+        switch ( command ) {
+          case DemoMessage.DEM_SYNCTICK:
+            console.log( 'dem_synctick' );
+            break;
+
+          case DemoMessage.DEM_STOP:
+            console.log( 'dem_stop' );
+            return;
+
+          case DemoMessage.DEM_CONSOLECMD:
+            console.log( 'dem_consolecmd' );
+            readRawData();
+            break;
+
+          case DemoMessage.DEM_DATATABLES:
+            console.log( 'dem_datatables' );
+            readRawData();
+            break;
+
+          case DemoMessage.DEM_STRINGTABLES:
+            console.log( 'dem_stringtables' );
+            readRawData();
+            break;
+
+          case DemoMessage.DEM_USERCMD:
+            console.log( 'dem_usercmd' );
+            readRawData();
+            break;
+
+          case DemoMessage.DEM_SIGNON:
+          case DemoMessage.DEM_PACKET:
+            console.log( 'dem_signon|dem_packet' );
+            readRawData();
+            break;
+
+          default:
+            console.log( 'default' );
+        }
+      }
     });
   });
 });
