1841e5e307641f0f43f0d1de1ed70071931d4b20
Raymond Zhou <somecrazyidiot@gmail.com>
2015-01-30 02:41:42 -0500

    bsp: Fix incorrect face generation.
    Replace THREE.BufferGeometry with Geometry. Simpler computation of face
    normals.

--- app/js/bsp/viewer.js
+++ app/js/bsp/viewer.js
@@ -8,58 +8,49 @@ function createBSPGeometry( bsp ) {
     faces
   } = bsp;
 
-  var geometry = new THREE.BufferGeometry();
-
-  var indices = [];
-  var positions = new Float32Array( 3 * vertexes.length );
+  var geometry = new THREE.Geometry();
 
   // Vertices.
-  var vertex;
   var i, il;
   for ( i = 0, il = vertexes.length; i < il; i++ ) {
-    vertex = vertexes[i];
-    positions[ 3 * i     ] = vertex.x;
-    positions[ 3 * i + 1 ] = vertex.y;
-    positions[ 3 * i + 2 ] = vertex.z;
+    geometry.vertices.push( new THREE.Vector3().copy( vertexes[i] ) );
   }
 
   // Faces.
   var edge;
   var surfedge;
   var face;
-  var v0, vi, vj;
+  var vi, vj, vk;
   var j;
   for ( i = 0, il = faces.length; i < il; i++ ) {
     face = faces[i];
 
-    for ( j = 0; j < face.numedges; j++ ) {
+    // Triangulate BSP faces (convex polygons).
+    for ( j = 0; j < face.numedges - 1; j++ ) {
       surfedge = surfedges[ face.firstedge + j ];
       edge = edges[ Math.abs( surfedge ) ];
 
       // Initial vertex.
       if ( !j ) {
-        v0 = surfedge >= 0 ? edge.v[0] : edge.v[1];
-        j++;
+        vi = surfedge >= 0 ? edge.v[0] : edge.v[1];
       } else {
         if ( surfedge >= 0 ) {
           // Positive direction.
-          vi = edge.v[0];
-          vj = edge.v[1];
+          vj = edge.v[0];
+          vk = edge.v[1];
         } else {
           // Negative direction.
-          vi = edge.v[1];
-          vj = edge.v[0];
+          vj = edge.v[1];
+          vk = edge.v[0];
         }
 
-        indices.push( v0 );
-        indices.push( vi );
-        indices.push( vj );
+        geometry.faces.push( new THREE.Face3( vi, vj, vk ) );
       }
     }
   }
 
-  geometry.addAttribute( 'index', new THREE.BufferAttribute( new Uint32Array( indices ), 1 ) );
-  geometry.addAttribute( 'position', new THREE.BufferAttribute( positions, 3 ) );
+  geometry.computeFaceNormals();
+  geometry.computeVertexNormals();
 
   return geometry;
 }
@@ -73,6 +64,7 @@ export function init( bsp ) {
   document.body.appendChild( container );
 
   renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer.setPixelRatio( window.devicePixelRatio );
   renderer.setSize( window.innerWidth, window.innerHeight );
   container.appendChild( renderer.domElement );
 
@@ -84,7 +76,7 @@ export function init( bsp ) {
   scene.add( camera );
 
   material = new THREE.MeshBasicMaterial({
-    color: 0xFFFFFF,
+    color: 0xffffff,
     wireframe: true
   });
 
